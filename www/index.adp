7409e9b014d1bd242d9db651d864ba7974d39834
Claudio Pasolini <claudio.pasolini@gmail.com>
2025-10-11 15:34:21 +0200

    Addesd explanatory messages

--- www/index.adp
+++ www/index.adp
@@ -4,11 +4,15 @@
 
 <if @mfa_mandatory_p@ true>
   <h2>The Multi Factor Authentication is mandatory for this site</h2>
+  <p>@user_status;literal@</p>
 </if>
 <else>
   <h2>The Multi Factor Authentication is optional</h2>
+    <p>@user_status;literal@</p>
   <formtemplate id="mfa"></formtemplate>
 </else>
+
+<p>In the context of this package <i>verified</i> means that the user has already done the initial setup with QR code and Base32 secret shared with Google Authenticator or one of the apps compliant with RFC 6238. <i>authorized</i> means that the user is already verified and entered a valid OTP.</p>
 
 <if @admin_p@ true>
     <p>As an admin you can <a href="toggle">toggle</a> the parameter that allows an administrator to set MFA as mandatory for everyone or selectable on a per-user basis.</p>
--- www/index.tcl
+++ www/index.tcl
@@ -15,7 +15,32 @@ set admin_p [acs_user::site_wide_admin_p -user_id $user_id]
 set page_title $title
 set context $page_title
 
-set verified_p [db_string get_user "select verified_p from mfa_users where user_id = :user_id" -default 0]
+if {![db_0or1row get_user "select verified_p, authorized_p from mfa_users where user_id = :user_id"]} {
+    set verified_p   "f"
+    set authorized_p "f"
+}
+
+set user_status {}
+if {$verified_p} {
+    if {$authorized_p} {
+	append user_status "You are already a verified and authorized user of the MFA. "
+    } else { 
+	append user_status "You are a verified but not yet authorized user of the MFA. "
+    }
+} else {
+    append user_status "You are not a verified user of the MFA. "
+}
+
+if {[mfa::parameter_get]} {
+    set mfa_mandatory_p 1
+} else {
+    set mfa_mandatory_p 0
+    if {$verified_p} {
+        append user_status "You can opt out from the MFA by selecting 'No' in the form below."
+    } else {
+        append user_status "You can start using it by selecting 'Yes' in the form below."
+    }
+}
 
 set buttons [list [list "Please choose one" new]]
 
@@ -57,11 +82,7 @@ ad_form -name mfa \
     ad_script_abort
 }
 
-if {[mfa::parameter_get]} {
-    set mfa_mandatory_p 1
-} else {
-    set mfa_mandatory_p 0
-}
+
 
 
 
